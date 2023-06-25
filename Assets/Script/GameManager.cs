84f540d5bc6cbce95db6452629386e1d2e0799b0
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-06-25 21:16:24 +0900

    経過時間の追加

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,17 +8,30 @@ public class GameManager : MonoBehaviour
     //オブジェクト
     GameObject gameOver;
     Text _gameover;
+
+    GameObject time;
+    Text time_tx;
+
+    float _time;
     // Start is called before the first frame update
     void Start()
     {
+        _time = 0;
+
+        time = GameObject.Find("Time");
+        time_tx = time.GetComponent<Text>();
+
         gameOver = GameObject.Find("GameOverText");
         _gameover = gameOver.GetComponent<Text>();
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _time += Time.deltaTime;
+        time_tx.text = "Time:" + _time.ToString("F2");
     }
 
     public void GameOver()
