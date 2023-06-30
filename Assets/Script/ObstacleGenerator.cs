519689c6f08c287066923c2d53b0c37ec098512b
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-06-30 23:03:14 +0900

    時間経過でスピードが上がる処理

--- Assets/Script/ObstacleGenerator.cs
+++ Assets/Script/ObstacleGenerator.cs
@@ -12,6 +12,18 @@ public class ObstacleGenerator : MonoBehaviour
     [SerializeField] float m_wallGenerateInterval = 2f;
     /// <summary>壁生成間隔を計るためのタイマー（秒）</summary>
     float m_timer = 0;
+    /// <summary>
+    /// 壁が早くなっていく秒数
+    /// </summary>
+    [SerializeField] float m_speedUpCount = 60f;
+    /// <summary>
+    ///　スピードアップ用カウント
+    /// </summary>
+    float m_speedUpTimer = 0f;
+    /// <summary>
+    /// 何秒早くするのかを決める変数
+    /// </summary>
+    [SerializeField] float m_speedUpNum = 0.25f;
 
     void Start()
     {
@@ -23,6 +35,7 @@ public class ObstacleGenerator : MonoBehaviour
     {
         m_timer += Time.deltaTime;
 
+        m_speedUpTimer += Time.deltaTime;
         // タイマーの値が生成間隔を超えたら
         if (m_timer > m_wallGenerateInterval)
         {
@@ -30,6 +43,13 @@ public class ObstacleGenerator : MonoBehaviour
             int index = Random.Range(0, m_wallPrefabs.Length);  // 配列からオブジェクトを選ぶためのインデックス（添字）をランダムに選ぶ
             GameObject go = Instantiate(m_wallPrefabs[index]);  // プレハブからオブジェクトを生成して、変数 go に入れる
             go.transform.position = m_position[index];   // 生成したオブジェクトの位置を定める
+        }
+
+        //スピードアップ
+        if(m_speedUpTimer > m_speedUpCount)
+        {
+            m_speedUpTimer = 0f;
+            m_wallGenerateInterval -= m_speedUpNum;
         }
     }
 }
